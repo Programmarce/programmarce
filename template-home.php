4fb70e8b18aea9090ea474976be2af7f6281265e
mmarecodivit <marcelo@dex360.net>
2023-01-29 19:38:42 -0300

    mejorando los estilos del template home

--- template-home.php
+++ template-home.php
@@ -5,9 +5,7 @@ mostrarRuta(0, __FILE__);
 get_header();
 ?>
 <main>
-    <style type="text/css">
-        .carousel-caption { top: 14rem; }
-    </style>
+    <!--slider ini-->
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -16,21 +14,21 @@ get_header();
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide/keyboar.jpg" class="d-block w-100" alt="..." style="height: 50rem;">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide/keyboar.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Creamos tu Pagina Web ya!</h5>
                     <p>Ponete en contacto con nosotros. <br> Empeza hoy a ver tu sitio Web.</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide/business.jpg" class="d-block w-100" alt="..." style="height: 50rem;">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide/business.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Inverti en tu Negocio Digital</h5>
                     <p>Aprovecha la tecnologia para llegar mas lejos.</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide/hands.jpg" class="d-block w-100" alt="..." style="height: 50rem;">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/slide/hands.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Tu negocio en manos de todo el mundo.</h5>
                     <p>mostrale a todos lo que ofreces.</p>
@@ -46,5 +44,79 @@ get_header();
             <span class="visually-hidden">Siguiente</span>
         </button>
     </div>
-    <div style="height: 500px"></div>
+    <!--slider fin-->
+    <div style="height: auto; margin: 1rem;">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col card_conten">
+                    <div class="card">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/banner-cartel-standar.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <div>
+                                <h5 class="card-title">Standar</h5>
+                            </div>
+                            <p class="card-text">Obtene una pagina web ya!<br> En pocos pasos y de forma sencilla.</p>
+                            <ul>
+                                <li>Dominio propio</li>
+                                <li>3 Secciones</li>
+                                <li>Certificado SSL</li>
+                                <li>SEO Basic</li>
+                                <li>Logo</li>
+                                <li>Redes sociales</li>
+                                <li>Galeria de imagenes</li>
+                                <li>Protección y Respaldo</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary bg-theme" style="border: none;">Consultar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col card_conten">
+                    <div class="card">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/banner-cartel-proweb.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Pro web</h5>
+                            <p class="card-text">Desarrollo, diseño y mantenimento de tu sitio activo.</p>
+                            <ul>
+                                <li>Dominio propio</li>
+                                <li>5 Secciones</li>
+                                <li>Formulario de Contacto</li>
+                                <li>Certificado SSL</li>
+                                <li>SEO Complete</li>
+                                <li>Logo</li>
+                                <li>Redes sociales</li>
+                                <li>Galeria de imagenes</li>
+                                <li>Protección y Respaldo</li>
+                                <li>Asesoramiento continuo</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary bg-theme" style="border: none;">Consultar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col card_conten">
+                    <div class="card">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/banner-cartel-premium.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Premium</h5>
+                            <p class="card-text">Desempeño dedicado a tu sitio. Implementación de SEO full, Seguridad y Mantenimiento premium</p>
+                            <ul>
+                                <li>Dominio propio</li>
+                                <li>5 Secciones</li>
+                                <li>Formulario de Contacto</li>
+                                <li>Certificado SSL</li>
+                                <li>SEO Premium</li>
+                                <li>Logo</li>
+                                <li>Redes sociales</li>
+                                <li>Galeria de imagenes</li>
+                                <li>Protección Total</li>
+                                <li>Antivirus y contrafuegos</li>
+                                <li>Asesoramiento full</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary bg-theme" style="border: none;">Consultar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
+<?php get_footer() ?>
