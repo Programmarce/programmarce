7f8a3a1b0c1fd395e0e36053c282001d78b29d69
mmarecodivit <marcelo@dex360.net>
2023-01-29 19:38:08 -0300

    iniciando footer

--- footer.php
+++ footer.php
@@ -3,6 +3,22 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 ?>
+<style>
+    .footer-container {
+        width: 100%;
+        height: 4rem;
+        background-color: black;
+    }
+    .footer-container p {
+        color: white;
+        text-align: center;
+        padding: 1rem;
+    }
+</style>
 <footer class="main-footer">
-
+    <div class="footer-container">
+        <div>
+            <p>&#169 Programmarce 2023 - Diseño Web</p>
+        </div>
+    </div>
 </footer>
