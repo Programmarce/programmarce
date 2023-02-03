ad8fe5a1eaf84f9b0a14a00d0e93f2504de6bfb4
mmarecodivit <marcelo@dex360.net>
2023-02-03 16:49:48 -0300

    ordenando el responsive header and footer

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@ $site_name = get_bloginfo('name');
 ?>
 <footer class="main-footer">
     <div class="footer-container">
-        <div>
-            <p>&#169 <?php echo $site_name." ".date("Y"); ?> - Web Design</p>
+        <div style="background-color: black;">
+            <p>&#169 <?php echo $site_name." - ".date("Y"); ?> <br> Web Design</p>
         </div>
     </div>
 </footer>
