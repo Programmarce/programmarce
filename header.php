8fb0df56bb9165b017cf5eb7acab03bbc146de96
marcelo <marcelo12mm@gmail.com>
2023-02-23 15:13:59 -0300

    add tag script recaptcha js

--- header.php
+++ header.php
@@ -21,6 +21,7 @@ if (!defined('ABSPATH')) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script async src="https://www.google.com/recaptcha/api.js">
     <?php wp_head(); ?>
 </head>
 <body>
